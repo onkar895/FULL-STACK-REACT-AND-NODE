15924ffaa031b397af3665e233e64c3f0c7fe48d
Onkar Karale <onkarkarale4@gmail.com>
2025-01-20 17:41:51 +0530

    Updated Navbar

---
+++ ReactJS/E-CommerceWebsite/client/SnapCart/src/Components/ProductList/ProductList.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import ProductCard from "./ProductCard"
+
+const ProductList = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    // Fetch data from API
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get('https://fakestoreapi.com/products')
+        console.log(res)
+        setProducts(res.data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    fetchProducts()
+  }, [])
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-36 gap-8 mx-8">
+      {
+        products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+export default ProductList
